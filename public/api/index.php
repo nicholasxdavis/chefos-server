01e01046454afaac77c288bbbc5606a032d5aa28
blacnova <147455788+nicholasxdavis@users.noreply.github.com>
2025-10-31 08:53:57 -0600

    Add files via upload

---
+++ public/api/index.php
@@ -0,0 +1,5 @@
+<?php
+// Front controller for /api/* when served from /app/public
+require __DIR__ . '/../../api.php';
+
+
---
+++ public/init.php
@@ -0,0 +1,5 @@
+<?php
+// Expose database initializer from web root
+require __DIR__ . '/../init.php';
+
+
